f0d50e94f34ba1144ed6827a0938b466182fae6e
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-01-27 16:36:31 +0100

    Add delete ingredient actions in IngredientService and IngredientController. .

--- src/test/java/guru/vanhaeren/recipe/controllers/IngredientControllerTest.java
+++ src/test/java/guru/vanhaeren/recipe/controllers/IngredientControllerTest.java
@@ -131,6 +131,16 @@ class IngredientControllerTest {
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/ingredient/3/show"));
+    }
 
+    @Test
+    public void testDeleteIngredient() throws Exception {
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/2/ingredient/3/delete")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredients"));
+
+        verify(ingredientService, times(1)).deleteById(anyLong(), anyLong());
     }
 }
